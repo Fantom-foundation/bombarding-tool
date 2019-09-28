b705c9dac04ebecff75f27fff6f0308c7e90958f
salamantos <m.b.volkov@yandex.ru>
2019-09-28 17:46:46 +0300

    add benchmark print

---
+++ src/load.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "sys/types.h"
+#include "sys/sysinfo.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+
+namespace blomb {
+    struct sysinfo memInfo;
+    static unsigned long long lastTotalUser, lastTotalUserLow, lastTotalSys, lastTotalIdle;
+
+    class Load {
+    public:
+        Load() {
+            FILE* file = fopen("/proc/stat", "r");
+            fscanf(file, "cpu %llu %llu %llu %llu", &lastTotalUser, &lastTotalUserLow,
+                   &lastTotalSys, &lastTotalIdle);
+            fclose(file);
+        }
+
+        double get_memory_usage() {
+            sysinfo (&memInfo);
+            auto totalVirtualMem = memInfo.totalram;
+            totalVirtualMem += memInfo.totalswap;
+            totalVirtualMem *= memInfo.mem_unit;
+
+            auto virtualMemUsed = memInfo.totalram - memInfo.freeram;
+            virtualMemUsed += memInfo.totalswap - memInfo.freeswap;
+            virtualMemUsed *= memInfo.mem_unit;
+
+            return (double)virtualMemUsed / (double)totalVirtualMem;
+        }
+
+        double get_cpu_usage() {
+            double percent;
+            FILE* file;
+            unsigned long long totalUser, totalUserLow, totalSys, totalIdle, total;
+
+            file = fopen("/proc/stat", "r");
+            fscanf(file, "cpu %llu %llu %llu %llu", &totalUser, &totalUserLow,
+                   &totalSys, &totalIdle);
+            fclose(file);
+
+            if (totalUser < lastTotalUser || totalUserLow < lastTotalUserLow ||
+                totalSys < lastTotalSys || totalIdle < lastTotalIdle){
+                //Overflow detection. Just skip this value.
+                percent = -1.0;
+            }
+            else{
+                total = (totalUser - lastTotalUser) + (totalUserLow - lastTotalUserLow) +
+                        (totalSys - lastTotalSys);
+                percent = total;
+                total += (totalIdle - lastTotalIdle);
+                percent /= total;
+                percent *= 100;
+            }
+
+            lastTotalUser = totalUser;
+            lastTotalUserLow = totalUserLow;
+            lastTotalSys = totalSys;
+            lastTotalIdle = totalIdle;
+
+            return percent;
+        }
+
+        void print_load(double cpu, double memory) {
+            printf ("CPU usage: %5.2f \n", cpu);
+            printf ("Memory usage: %5.2f \n", memory);
+        }
+    };
+}
